897c11ff2f00573e025de17e5194846774e2fb2a
DuvanCalderon <79523677+DuvanCalderon@users.noreply.github.com>
2023-09-06 12:44:57 -0500

    Add files via upload

---
+++ Ejercicio22.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio22;
+
+import java.util.Scanner;
+
+public class Ejercicio22 {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el nombre del empleado: ");
+        String nombre = scanner.nextLine();
+        
+        System.out.print("Ingrese el salario básico por hora: ");
+        double salarioPorHora = scanner.nextDouble();
+        
+        System.out.print("Ingrese el número de horas trabajadas en el mes: ");
+        int horasTrabajadas = scanner.nextInt();
+
+        
+        double salarioMensual = salarioPorHora * horasTrabajadas;
+
+        
+        if (salarioMensual > 450000) {
+            System.out.println("Nombre del empleado: " + nombre);
+            System.out.println("Salario mensual: $" + salarioMensual);
+        } else {
+            System.out.println("Nombre del empleado: " + nombre);
+        }
+    }
+}
+
+   
+    
